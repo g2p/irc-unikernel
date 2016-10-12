0786cb6ef05de2701b37b17ae4c8cc1d1d221625
Gabriel <g2p.code@gmail.com>
2016-10-12 21:47:17 +0200

    WIP Write a functor porting ocaml-irc-client to Mirage

--- config.ml
+++ config.ml
@@ -1,15 +1,15 @@
 open Mirage
 
 let client =
-  let libraries = [ "mirage-http"; "duration" ] in
-  let packages = [ "mirage-http"; "duration" ] in
+  let packages = [ "mirage-http"; "duration"; "irc-client" ] in
+  let libraries = [ "mirage-http"; "duration"; "irc-client"; "lwt.ppx" ] in
   foreign
     ~libraries ~packages
-    "Unikernel.Client" @@ time @-> console @-> resolver @-> conduit @-> job
+    "Unikernel.Client" @@ time @-> console @-> stackv4 @-> resolver @-> conduit @-> job
 
 let () =
   let stack = generic_stackv4 tap0 in
   let res_dns = resolver_dns stack in
   let conduit = conduit_direct stack in
-  let job =  [ client $ default_time $ default_console $ res_dns $ conduit ] in
+  let job =  [ client $ default_time $ default_console $ stack $ res_dns $ conduit ] in
   register "irc" job
--- unikernel.ml
+++ unikernel.ml
@@ -2,7 +2,36 @@ open Lwt.Infix
 open V1_LWT
 open Printf
 
-module Client (T: TIME) (C: CONSOLE) (RES: Resolver_lwt.S) (CON: Conduit_mirage.S) = struct
-  let start _time c res (ctx:CON.t) =
+let ns = "8.8.8.8"
+
+exception ConnectionFailure
+exception NotImplemented
+
+module Client (T: TIME) (C: CONSOLE) (STACK: STACKV4) (RES: Resolver_lwt.S) (CON: Conduit_mirage.S) = struct
+  let start _time c stack res (ctx:CON.t) =
+
+    let module Irc_io = struct
+      type 'a t = 'a Lwt.t
+      let (>>=) = Lwt.bind
+      let return = Lwt.return
+      let iter = Lwt_list.iter_s
+
+      type file_descr = STACK.TCPV4.flow
+      type inet_addr = STACK.ipv4addr
+
+      let open_socket addr port =
+        match%lwt STACK.TCPV4.create_connection (STACK.tcpv4 stack) (addr, port) with
+        |`Error `Refused -> Lwt.fail ConnectionFailure
+        |`Error `Timeout -> Lwt.fail ConnectionFailure
+        |`Error `Unknown st -> Lwt.fail ConnectionFailure
+        |`Ok flow -> Lwt.return flow
+      let close_socket = STACK.TCPV4.close
+
+      let read socket buf off len = Lwt.fail NotImplemented
+      let write socket buf off len = Lwt.fail NotImplemented
+
+      let gethostbyname name = Lwt.fail NotImplemented
+    end in
+    let module Irc = Irc_client.Make(Irc_io) in
     Lwt.return ()
 end
